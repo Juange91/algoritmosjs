cda82baac7fec54c55aa7b5cb79e99028f1b904f
Juange91 <91135380+Juange91@users.noreply.github.com>
2022-03-24 11:05:55 -0500

    commit inicial

---
+++ main.js
@@ -0,0 +1,55 @@
+
+function suma(){
+var a =0;
+var b =0;
+var Resultado = 0;
+a = parseInt(prompt("Porfavor ingrese el primer valor"));
+b = parseInt(prompt("Porfavor ingrese el segundo valor"));
+Resultado = a+b;
+alert("El resultado de la suma es: "+Resultado);
+
+}
+
+function PulgaMetros(){
+
+    var m = 0;
+    var p = 0;
+
+    m = parseInt(prompt("Ingrese los metros a convertir en pulgadas"));
+    p = m*39,37
+
+alert("Los metros convertido a pulgadas es:"+p)
+}
+
+function suma2(){
+    var a =0;
+    var b =0;
+    var suma = 0;
+var resta = 0;
+var multiplicacion =0;
+var divicion =0;
+
+    a = parseInt(prompt("Porfavor ingrese el primer valor"));
+    b = parseInt(prompt("Porfavor ingrese el segundo valor"));
+    suma = a+b;
+    resta = a-b;
+    multiplicacion = a*b;
+    divicion = a/b;
+
+    alert("El resultado de la suma es: "+suma);
+    alert("El resultado de la resta es: "+resta);
+    alert("El resultado de la multiplicacion es: "+multiplicacion);
+    alert("El resultado de la divicion es: "+divicion);
+}
+ 
+function Areatriangulo(){
+    b =0;
+    a =0;
+h =0;
+b = parseInt(prompt("Ingrese la base del triangulo"));
+h = parseInt(prompt("Ingrese la altura del triangulo"));
+a =b*h/2
+
+alert("El area del triangulo es: "+a);
+}
+funtion 
